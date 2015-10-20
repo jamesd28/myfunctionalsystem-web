83866a59467202d14ba3cc1c2aaa11519c860e66
David James <jamesd28@mymacewan.ca>
2015-10-20 17:28:58 -0600

    Added helmet.js for security.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 // Dependency variables
 var express = require('express'),
-	morgan = require('morgan');
+	morgan = require('morgan'),
+	helmet = require('helmet');
 
 // Webserver (express) initialization.
 var	app = express();
 
 // Webserver Configuration.
-app.use(morgan('dev'));
+app.use(morgan('dev')); // Console Logging
+app.use(helmet()); // Security :D
 
 // Routes
 app.get('/', function(req, res){
